a48bbb0ecb4c5e0131dd36020debcb01d4090b66
Aleksandr Musorin <aleksandr.musorin@semrush.com>
2024-11-25 18:40:46 +0100

    Load .nvimrc.lua file on start

--- init.lua
+++ init.lua
@@ -14,3 +14,15 @@ require "user.bufferline"
 require "user.fidget"
 require "user.lazygit"
 require "user.whichkey"
+
+local function load_project_config()
+  local local_config = vim.fn.getcwd() .. "/.nvimrc.lua"
+  if vim.fn.filereadable(local_config) == 1 then
+    dofile(local_config)
+  end
+end
+
+-- Load the project-specific config on entering the directory
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = load_project_config,
+})
