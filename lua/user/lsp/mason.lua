ea58757ef412094dc8c2b04e7e03c1e4d086fa0e
Aleksandr Musorin <aleksandr.musorin@semrush.com>
2025-05-19 17:38:33 +0200

    Fix mason configuration after mason-lspconfig upgrade
    See: https://github.com/mason-org/mason-lspconfig.nvim/blob/main/CHANGELOG.md#200-2025-05-06

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -1,12 +1,15 @@
+local mason = require("mason")
+local mason_lspconfig = require("mason-lspconfig")
+local handlers = require("user.lsp.handlers")
+local lspconfig = require("lspconfig")
+
 local servers = {
   "lua_ls",
   "pyright",
   "jsonls",
   "clangd",
   "jdtls",
-  -- Prevent starting up a conflicting rust_analyzer
-  -- https://github.com/mrcjkb/rustaceanvim/discussions/94
-  -- "rust_analyzer",
+  "rust_analyzer",
   "bashls",
   "gopls",
   "dockerls",
@@ -22,34 +25,22 @@ local settings = {
       package_uninstalled = "◍",
     },
   },
+  ensure_installed = servers,
   log_level = vim.log.levels.INFO,
   max_concurrent_installers = 4,
 }
 
-require("mason").setup(settings)
-require("mason-lspconfig").setup({
-  ensure_installed = servers,
-  automatic_installation = true,
-})
+mason.setup(settings)
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-  return
-end
-
-local opts = {}
-
-for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("user.lsp.handlers").on_attach,
-    capabilities = require("user.lsp.handlers").capabilities,
+for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
+  local opts = {
+    on_attach = handlers.on_attach,
+    capabilities = handlers.capabilities,
   }
 
-  server = vim.split(server, "@")[1]
-
-  local require_ok, conf_opts = pcall(require, "user.lsp.settings." .. server)
-  if require_ok then
-    opts = vim.tbl_deep_extend("force", conf_opts, opts)
+  local ok, custom_opts = pcall(require, "user.lsp.settings." .. server)
+  if ok then
+    opts = vim.tbl_deep_extend("force", opts, custom_opts)
   end
 
   lspconfig[server].setup(opts)
